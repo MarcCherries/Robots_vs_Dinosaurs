c5f6a4f394433801c45caf94701a88ae191accd9
marccherries <chrisc1983@gmail.com>
2022-03-03 09:23:58 -0800

    set up a skeleton for robots vs dinos project with all init, methods(without logic), and instance variables

---
+++ Dinosaur.py
@@ -0,0 +1,9 @@
+class Dinousaur:
+
+    def __init__(self, name, attack_power):
+        self.name = name
+        self.attack_power = attack_power
+        health = 100  
+
+    def attack(self, robot):
+        pass
---
+++ Robots.py
@@ -0,0 +1,12 @@
+from 
+
+
+class Robots:
+    
+    def __init__(self, name, health, weapon):
+        self.name = name
+        self.health = 100
+        self.weapon = Weapon()
+        
+    def attack(self, dinosaur):
+        pass
